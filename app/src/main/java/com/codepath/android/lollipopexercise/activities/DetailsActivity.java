853b5e55e2c434f82e5ff2d7c13777458856c312
pbabu <prabhu.babu@elementum.com>
2016-09-03 23:51:09 -0700

    added circular reveal support

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,12 +1,19 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
+import android.transition.Transition;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,6 +29,9 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView tvName;
     private TextView tvPhone;
     private View vPalette;
+    private FloatingActionButton fab;
+
+    private Transition.TransitionListener mEnterTransitionListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,17 @@ public class DetailsActivity extends AppCompatActivity {
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         vPalette = findViewById(R.id.vPalette);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+        fab.setVisibility(View.INVISIBLE);
 
         // Extract contact from bundle
         mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
@@ -66,15 +87,97 @@ public class DetailsActivity extends AppCompatActivity {
         Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(target);
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
+
+        mEnterTransitionListener = new Transition.TransitionListener() {
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                enterReveal();
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        };
+        getWindow().getEnterTransition().addListener(mEnterTransitionListener);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home) {
-            finishAfterTransition();
+            exitReveal();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void enterReveal() {
+        final int width = fab.getWidth();
+        final int height = fab.getHeight();
+        int cx = width/2;
+        int cy = height/2;
+        int finalRadius = Math.max(width, height)/2;
+        final Animator anim = ViewAnimationUtils.createCircularReveal(fab, cx, cy, 0, finalRadius);
+        fab.setVisibility(View.VISIBLE);
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                getWindow().getEnterTransition().removeListener(mEnterTransitionListener);
+            }
+        });
+        anim.start();
+
+    }
+
+    private void exitReveal() {
+        final int width = fab.getWidth();
+        final int height = fab.getHeight();
+        int cx = width/2;
+        int cy = height/2;
+        int initialRadius = height/2;
+        final Animator anim = ViewAnimationUtils.createCircularReveal(fab, cx, cy,initialRadius,0);
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                super.onAnimationEnd(animator);
+                fab.setVisibility(View.INVISIBLE);
+                finishAfterTransition();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        anim.start();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        exitReveal();
     }
 
 }
